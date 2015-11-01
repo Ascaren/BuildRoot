386062d76202acb959dada114f26252b77f58b92
Kamil <altaran007@gmail.com>
2015-11-01 11:18:54 +0100

    first

---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "multiplication.h"
+#include <stdio.h>
+
+int main(){
+	int value = multiplication(2,3);
+	printf("Wynik: %d",value);
+	return 0;
+}
---
+++ src/multiplication.c
@@ -0,0 +1,6 @@
+#include "multiplication.h"
+#include <stdio.h>
+
+int multiplication(int a,int b){
+	return a*b;
+}
